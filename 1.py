573b100b53e47d59a6c6977c79facf921c09ca9a
Prayansh <iprayanshmishra15@gmail.com>
2022-03-03 23:59:03 +0530

    This is the Intial commit for my Python repo

---
+++ 1.py
@@ -0,0 +1,5 @@
+'''
+Author - Prayansh Mishra
+***written on - 3-3-22***
+'''
+print("Hello World") #a simple code that prints the value provided to it into the console as suggested by its name#
---
+++ practiseset_01.py
@@ -0,0 +1,14 @@
+'''
+Author - Prayansh Mishra
+date-3-3-22
+'''
+# Q1)print('''Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.''') the take away here is for multiplel line display we add ''' to the code not ""
+
+# Q3) from playsound import playsound
+# playsound('C:\\Users\\Prayansh\\Downloads\\Laguanz.com - The Script - The Man Who Can’t Be Moved (Official Video).mp3')
+
+#Q4) import os
+# print(os.listdir())
